7fbf3587e7133e73c78f40931c0c364c2bf64d48
Brien Boudreau <brienboudreau@icloud.com>
2023-03-29 19:05:50 -0400

    home screen changes when logged in

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from '../login/auth.service';
 
 @Component({ templateUrl: 'home.component.html',
 styleUrls: ["./home.component.css"] })
 
-export class HomeComponent {}
+export class HomeComponent {
+
+    constructor(private authService: AuthService) {}
+
+    public isLoggedIn(): boolean {
+        return this.authService.isLoggedIn();
+    }
+
+}
