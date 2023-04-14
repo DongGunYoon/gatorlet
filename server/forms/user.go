2d115fc279eee4244ec8510ff516ef5826da3ca4
DongGunYoon <donggun961108@gmail.com>
2023-04-14 22:32:28 +0900

    Upt. update the user form file to have base form

--- server/forms/user.go
+++ server/forms/user.go
@@ -1,12 +1,15 @@
 package forms
 
-type RegisterForm struct {
+type BaseUserForm struct {
 	Email    string `json:"email" binding:"required"`
-	Name     string `json:"name" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
+type RegisterForm struct {
+	BaseUserForm
+	Name string `json:"name" binding:"required"`
+}
+
 type LoginForm struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	BaseUserForm
 }
